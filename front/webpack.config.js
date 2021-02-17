71a0b427b689282c24e00da332c1a6af8062db97
Pyry <pyr.hel@gmail.com>
2021-02-17 03:14:13 +0200

    added .gitignore and removed node_modlues directory from repo

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -2,31 +2,39 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 
 module.exports = {
     mode: 'development',
+    //mode: 'production',
     entry: {
         index: './src/index.js',
         print: './src/print.js',
+        manifest: '/src/manifest.webmanifest'
     },
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
-      },    
+    },
     plugins: [
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
         new HtmlWebpackPlugin({
             title: 'Development',
         }),
-      ],
-    
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
+    ],
+
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
-        rules: [
-        ],
+        rules: [],
     },
 };
